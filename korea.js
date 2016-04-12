7cb80f15d35288311907b412a3b52e4828f11ef6
Alex Ung <alexung.ucsb@gmail.com>
2016-04-12 14:31:33 +0900

    initialize korea

---
+++ korea.js
@@ -0,0 +1,32 @@
+function todoKoreaWithDerrickandHelen() {
+
+  about 3 hours-ish for ingwansan hike.  bring snacks + lots of water for it because of the length.
+
+
+  1. inwangsan hike (To get to the hike, you can get off at the Dognimmun subway station and take exit 1. Go uphill to the right and turn right on Tongil-ro 18 road. At the sharp turn in the road, head straight through the temple gate up the insanely steep road. Continue uphill.) OR
+  (This uphill walk is steep but really rewarding and a great way to get away from the busy city of Seoul - if only for a few hours. You will see Buddhist temples, a shamanist shrine, and enjoy a great view. Take subway Line 3 to Dongnimmun station - leave by Exit 2 and turn down the first winding alley on your left. Walk uphill past grocery shops for about 10 minutes and you will see the temple gateway on your left. If you ask anyone along the way they will be happy to provide you with directions - even if they speak no English. Everyone knows why you are there and they are happy to point you in the right direction. Along the way you will see traditional houses and temples with murals that depict the Buddhist way of life. Eventually you will arrive at the Zen rocks where women come to pray for sons. A young woman was there when I arrived so I waited for her to finish before looking around. A big surprise was finding exercise equipment dispersed around the hillside for you to use. Yes, I am talking treadmills and stairmasters - very strange! You will see candles, incense, and other offerings set into the hillside as you make your way to the various shrines. Wear good shoes since the climb is steep and the path can be rocky.)
+
+  2.
+  myeongdong kyoja (famous handmade noodles and dumplings in seoul)
+
+   Myeongdong Shopping Street (A visit to Seoul wouldnt be complete without a visit to Myeongdong. This all-encompassing neighborhood is one of Seouls ultimate places to meet up, shop, eat, and chill. Stores, cafes and restaurants are all stacked atop each other, creating seemingly endless rows and making the area one of Seouls top attractions.)
+
+  3. dinner at gwangjang night market. soju etc etc.
+
+  4. home!!
+
+
+}
+
+
+function todoApr12() {
+
+  1. namdaemun market
+
+  2. coex mall
+
+  3. Seoul Arts Center
+
+  4. hongdae nightlife
+
+}
