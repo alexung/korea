982ee9f880f8d614d94e2c758043b3db8f0f3e2e
Alex Ung <alexung.ucsb@gmail.com>
2016-04-17 13:48:19 +0900

    japan planning

---
+++ japan.js
@@ -0,0 +1,21 @@
+1. scotchbank: souffle pancake
+3 Chome-26-17 Shibuya, Shibuya-ku, Tōkyō-to 150-0002
+
+2. Toyoko Noren-gai: 83 different eateries inside
+1-12-2 Dogenzaka, Shibuya-ku, Tokyo
+Tokyo
+
+3. Yamashita Shoten: Magazine store
+3-19-1 Shibuya, Shibuya-ku, Tokyo
+Tokyo
+
+4. Tower Records Shibuya: vinyls + books
+1-22-14 Jinnan, Shibuya-ku
+Tokyo
+
+// Todo later:
+// - Nagi Sokudo: vegetarian place Mon-sat 12noon-3pm, 6pm - 11pm; Sun 12pm-3pm
+//   15-10 Uguisudanicho, Shibuya-ku Tokyo
+
+
+
